7a776d3264fb8314fe1b14e5383a3789ced5c125
MrEleven <mr.amirprogrammer@gmail.com>
2022-07-26 23:54:40 +0430

    fix room bug version, fix dao conflict strategy

--- app/src/main/java/ir/interview/idmb/data/database/dao/MovieDao.kt
+++ app/src/main/java/ir/interview/idmb/data/database/dao/MovieDao.kt
@@ -2,7 +2,7 @@ package ir.interview.idmb.data.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import ir.interview.idmb.data.database.entities.MovieDetailEntity
 import ir.interview.idmb.data.database.entities.MovieEntity
@@ -16,9 +16,9 @@ interface MovieDao {
     @Query("SELECT * FROM movie_details WHERE imdbID=:movieId")
     suspend fun get(movieId: String): MovieDetailEntity?
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovies(movies: List<MovieEntity>)
 
-    @Insert(onConflict = REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovieDetail(movie: MovieDetailEntity)
 }
